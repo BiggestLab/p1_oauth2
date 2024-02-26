465221c055863fd11165c93c6a68deb5648b9038
Kirit Sælensminde <kirit@felspar.com>
2024-02-26 16:18:44 +0700

    Add debug print

--- src/oauth2.erl
+++ src/oauth2.erl
@@ -118,7 +118,9 @@ authorize_password(User, Client, Scope, Ctx0) ->
                             -> {ok, {appctx(), auth()}} | {error, error()}.
 authorize_password(User, Client, RedirUri, Scope, Ctx0) ->
     case ?BACKEND:get_client_identity(Client,Ctx0) of
-      {error, _}   ->{error, invalid_client};
+      {error, _}   ->
+         io:format("\nauthorize_password ~p\n", [Client]),
+        {error, invalid_client};
       {ok,{Ctx1,C}} ->
         case ?BACKEND:verify_redirection_uri(C, RedirUri, Ctx1) of
           {error, _}      -> {error, invalid_client};
